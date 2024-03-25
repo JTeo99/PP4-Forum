b3bd5ce5f39a0a55b27a116ac36f25aa05b588ed
JTeo99 <jteo99@hotmail.com>
2024-03-25 19:15:57 +0000

    base comment model

--- blog/migrations/0003_post_updated_on.py
+++ blog/migrations/0003_post_updated_on.py
@@ -16,3 +16,19 @@ class Migration(migrations.Migration):
             field=models.DateTimeField(auto_now=True),
         ),
     ]
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
