14bdb60bb2372d2de96f9b557d50738144ed6c80
JTeo99 <jteo99@hotmail.com>
2024-03-25 17:02:41 +0000

    secret keys added

--- forum/settings.py
+++ forum/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q7!6b0o5i=9)#_e*1(m98v4uqp@0==#zkwunp^)gs3g@d90kmu'
+SECRET_KEY = 'Dvzx673askens3k4n45k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
