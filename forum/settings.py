c1edbab465e8e69ea8c3a1f2bd0725e411c88d83
JTeo99 <jteo99@hotmail.com>
2024-03-25 16:22:19 +0000

    added heroku to site

--- forum/settings.py
+++ forum/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-q7!6b0o5i=9)#_e*1(m98v4uqp@0==#zkwunp^)gs3g@d90kmu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jteo99-pp4-forum-v9ga2b1gfs.us1.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-jteo99-pp4-forum-v9ga2b1gfs.us1.codeanyapp.com',
+                '.herokuapp.com']
 
 
 # Application definition
